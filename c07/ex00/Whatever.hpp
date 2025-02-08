683678f96c06160b7cde944fc44f6e5de7f054e4
Seyed Ali Sanei <ssanei@3-D-8.42heilbronn.de>
2025-02-08 14:31:33 +0100

    Add template functions according to subject.

---
+++ c07/ex00/Whatever.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssanei <ssanei@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 14:24:34 by ssanei            #+#    #+#             */
+/*   Updated: 2025/02/08 14:24:37 by ssanei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#define EXIT_SUCCESS 0
+
+#include "Utils.hpp"
+
+
+typedef std::string str;
+
+template<typename T> void	swap(T &arg1, T &arg2);
+
+template<typename T> T	min(T arg1, T arg2);
+
+template<typename T> T	max(T arg1, T arg2);
+
+
+#endif
+
