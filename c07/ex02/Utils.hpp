03d5abd29828e57ecc23dfa8443841d67bbd966f
Seyed Ali Sanei <ssanei@3-D-8.42heilbronn.de>
2025-02-08 23:47:21 +0100

    Add files from previous exercise.

---
+++ c07/ex02/Utils.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssanei <ssanei@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:57:27 by ssanei            #+#    #+#             */
+/*   Updated: 2025/02/01 16:07:29 by ssanei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <iostream>
+# include <string>
+
+typedef std::string str;
+
+void	print_message(str msg);
+
+#endif
