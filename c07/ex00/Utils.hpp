8c25583ae31d771ecdac569a7ae418a955158c31
Seyed Ali Sanei <ssanei@3-D-8.42heilbronn.de>
2025-02-08 13:56:25 +0100

    Add files from prevoius exercises & start the project.

---
+++ c07/ex00/Utils.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssanei <ssanei@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 13:55:20 by ssanei            #+#    #+#             */
+/*   Updated: 2025/02/08 13:55:22 by ssanei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <iostream>
+# include <string>
+
+typedef std::string str;
+
+void	print_message(str msg);
+
+#endif
